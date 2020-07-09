0c0dbebc480938640775a2ff5146378cdb488dd8
emanuelosva <emanuelosva@gmail.com>
2020-07-09 11:43:21 -0500

    Link to home

--- frontEnd/src/components/Header.jsx
+++ frontEnd/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../assets/styles/components/Header.scss';
 
 import logo from '../assets/static/logo-platzi-video-BW2.png';
@@ -6,7 +7,9 @@ import userIcon from '../assets/static/user-icon.png';
 
 const Header = () => (
   <header className='header'>
-    <img className='header__img' src={logo} alt='Platzi Video' />
+    <Link to='/'>
+      <img className='header__img' src={logo} alt='Platzi Video' />
+    </Link>
     <div className='header__menu'>
       <div className='header__menu--profile'>
         <img src={userIcon} alt='' />
