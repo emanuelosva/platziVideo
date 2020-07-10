ded000d428051023d36621317fa76dc48f583e7c
emanuelosva <emanuelosva@gmail.com>
2020-07-10 15:49:10 -0500

    Serve react by express js

--- website/src/server/server.js
+++ website/src/server/server.js
@@ -22,7 +22,21 @@ if (config.env !== 'production') {
 };
 
 app.get('*', (req, res) => {
-  res.send({ hello: 'express' });
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="assets/app.css" type="text/css" />
+        <title>Platzi video</title>
+      </head>
+      <body>
+        <div id="app"></div>
+        <script src="assets/app.js" type="text/javascript"></script>
+      </body>
+    </html>
+  `);
 });
 
 app.listen(PORT, (err) => {
