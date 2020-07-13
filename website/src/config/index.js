d68b30ce03e14edfb634fa70c93413634d814915
emanuelosva <emanuelosva@gmail.com>
2020-07-13 15:29:10 -0500

    Choose port accordign to the env

--- website/src/config/index.js
+++ website/src/config/index.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 
+// Change port by env
+let PORT;
+process.env.NODE_ENV === 'production' ?
+  PORT = process.env.PORT_PROD :
+  PORT = process.env.PORT;
+
 module.exports = {
-  port: process.env.PORT,
+  port: PORT,
   env: process.env.NODE_ENV,
 };
