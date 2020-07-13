69f8c1cf703b4d084cffcef1c28f14cfbd9a70a0
emanuelosva <emanuelosva@gmail.com>
2020-07-13 18:40:16 -0500

    Add bundle splitting with vendor files

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -28,6 +28,25 @@ module.exports = {
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin()],
+    splitChunks: {
+      chunks: 'async',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          name: 'vendors',
+          chunks: 'all',
+          reuseExistingChunk: true,
+          priority: 1,
+          filename: isDev ? 'assets/vendor.js' : 'assets/vendor-[hash].js',
+          enforce: true,
+          test(module, chunks) {
+            const name = module.nameForCondition && module.nameForCondition();
+            return chunks.some((chunk) => (
+              chunks.name !== 'vendors' && /[\\/]node_modules[\\/]/.test(name)));
+          },
+        },
+      },
+    },
   },
   module: {
     rules: [
