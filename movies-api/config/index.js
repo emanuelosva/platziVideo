5ac8acd110da46001957dde81f81babe012d3e0d
emanuelosva <emanuelosva@gmail.com>
2020-07-14 08:34:29 -0500

    DB credential added

--- movies-api/config/index.js
+++ movies-api/config/index.js
@@ -3,4 +3,10 @@ require('dotenv').config();
 module.exports = {
   dev: process.env.NODE_ENV !== 'production',
   port: process.env.PORT || '3000',
+  db: {
+    name: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    cluster: process.env.DB_CLUSTER,
+  },
 };
