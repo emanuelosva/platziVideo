01ab3f6c3881f55e23d4240c63f381a8f0d14268
emanuelosva <emanuelosva@gmail.com>
2020-07-14 17:12:37 -0500

    Boom errors implementation

--- movies-api/src/network/middleware/errorHandlers.js
+++ movies-api/src/network/middleware/errorHandlers.js
@@ -4,12 +4,12 @@
 */
 
 const config = require('../../../config');
+const boom = require('@hapi/boom');
 const { errorResponse } = require('../response');
 
 // Utils
 const withErrorStack = (error, stack) => {
   if (config.dev) return { error, stack };
-
   return error;
 };
 
@@ -19,12 +19,31 @@ const logErrors = (err, req, res, next) => {
   next(err);
 };
 
+const wrapErrors = (err, req, res, next) => {
+  // For personalized errors (it does not have [message])
+
+  if (!err.message || err.isBoom) {
+    next(err)
+  }
+
+  next(boom.badImplementation(err));
+};
+
 const errorHandler = (err, req, res, next) => {
-  const error = withErrorStack(err.message || err, err.stack)
-  errorResponse(req, res, error, err.status || 500);
+  // Only personalized errors are not boom
+
+  if (!err.isBoom) {
+    const error = withErrorStack(err, err.stack);
+    errorResponse(req, res, error, err.status || 500);
+  } else {
+    const { output: { statusCode, payload } } = err;
+    const error = withErrorStack(payload.message, err.stack);
+    errorResponse(req, res, error, statusCode || 500);
+  }
 };
 
 module.exports = {
   logErrors,
+  wrapErrors,
   errorHandler,
 };
