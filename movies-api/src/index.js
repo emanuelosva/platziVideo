e3171afe54d041a38a060387d3c52bb424d9e986
emanuelosva <emanuelosva@gmail.com>
2020-07-14 07:39:43 -0500

    Implementation of API-Movie router

--- movies-api/src/index.js
+++ movies-api/src/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const config = require('../config');
+const moviesRouter = require('./routes/movies');
 
-// App definition
+// --- App definition ---
 const app = express();
 
-// Routes
-app.get('/', (req, res) => res.send('Hello'));
+// --- Routes ---
+moviesRouter(app);
 
-// Server initilization
+// --- Server initilization ---
 app.listen(config.port, (err) => {
   if (err) throw new Error('Server Error');
   console.log(`Server running on http://localhost:${config.port}`);
