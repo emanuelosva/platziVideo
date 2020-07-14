fc7cf2e79274d9971cc02f8be3b95c1d108d9344
emanuelosva <emanuelosva@gmail.com>
2020-07-14 12:23:57 -0500

    Add mongo connection object lib

---
+++ movies-api/src/lib/db/mongo.js
@@ -0,0 +1,114 @@
+/**
+ * @fileoverview Mongo connection
+*/
+
+const { MongoClient, ObjectId } = require('mongodb');
+const config = require('../../../config');
+
+// Connection credential
+const USER = encodeURIComponent(config.db.user);
+const PASSWORD = encodeURIComponent(config.db.password);
+const DB_NAME = config.db.name;
+const DB_CLUSTER = config.db.cluster;
+const Q_CONF = 'retryWrites=true&w=majority';
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${DB_CLUSTER}/${DB_NAME}?${Q_CONF}`;
+
+class MongoLib {
+  constructor(collection) {
+    this.client = new MongoClient(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    this.dbName = DB_NAME;
+    this.collection = collection;
+    this.connection = undefined;
+    this.db = this.connect();
+  }
+
+  async connect() {
+    if (this.connection) return this.connection;
+
+    try {
+      const client = await this.client.connect();
+      this.connection = client.db(this.dbName);
+      console.log('[mongoDb] -> connected successfully');
+      return this.connection
+    } catch (error) {
+      console.error(`[mongoDb] -> ${error}`);
+      process.exit(1);
+    }
+  }
+
+  async getAll(query) {
+    try {
+      const result = await this.db
+        .collection(this.collection)
+        .find(query)
+        .toArray();
+
+      return result;
+    } catch (error) {
+      console.error(`[mongoDb] -> ${error}`)
+      process.exit(1);
+    }
+  }
+
+  async get(id) {
+    try {
+      const result = await this.db
+        .collection(this.collection)
+        .findOne({ _id: ObjectId(id) });
+
+      return result;
+    } catch (error) {
+      console.error(`[mongoDb] -> ${error}`)
+      process.exit(1);
+    }
+  }
+
+  async create(data) {
+    try {
+      const result = await this.db
+        .collection(this.collection)
+        .insertOne(data);
+
+      return result;
+    } catch (error) {
+      console.error(`[mongoDb] -> ${error}`)
+      process.exit(1);
+    }
+  }
+
+  async update(id, data) {
+    try {
+      const result = await this.db
+        .collection(this.collection)
+        .updateOne(
+          { _id: ObjectId(id) },
+          { $set: data },
+          { $upsert: true },
+        );
+
+      return result;
+    } catch (error) {
+      console.error(`[mongoDb] -> ${error}`)
+      process.exit(1);
+    }
+  }
+
+  async delete(id) {
+    try {
+      const result = await this.db
+        .collection(this.collection)
+        .deleteOne({ _id: ObjectId(id) });
+
+      return result;
+    } catch (error) {
+      console.error(`[mongoDb] -> ${error}`)
+      process.exit(1);
+    }
+  }
+};
+
+module.exports = MongoLib;
