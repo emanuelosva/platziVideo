ad00d4b7695cd8f55ca15d7fa4e5f69acbc6b386
emanuelosva <emanuelosva@gmail.com>
2020-07-15 19:27:08 -0500

    Separte schemas from object

--- api/src/schemas/users.js
+++ api/src/schemas/users.js
@@ -7,17 +7,29 @@ const Joi = require('@hapi/joi');
 // --- Schemas ---
 
 const userIdSchema = Joi.string().regex(/^[0-9a-fA-F]{24}$/);
+const userNameSchema = Joi.string().max(100);
+const userEmailSchema = Joi.string().email();
+const userPasswordSchema = Joi.string().min(6);
 
 // --- Object Schemas ---
 
 const createUserSchema = {
-  name: Joi.string().max(100).required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
+  name: userNameSchema.required(),
+  email: userEmailSchema.required(),
+  password: userPasswordSchema.required(),
+  isAdmin: Joi.boolean(),
+};
+
+const updateUserSchema = {
+  name: userNameSchema,
+  email: userEmailSchema,
+  password: userPasswordSchema,
   isAdmin: Joi.boolean(),
 };
 
 module.exports = {
   userIdSchema,
+  userEmailSchema,
   createUserSchema,
+  updateUserSchema,
 };
